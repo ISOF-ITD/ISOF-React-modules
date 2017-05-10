af7bf03d67fd70011aa4e1ac6a415f57144d89d2
traustid <traustid@gmail.com>
2017-05-10 08:24:47 +0200

    Flyttade CheckBoxList till ISOF-React-modules

---
+++ components/views/CheckBoxList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class CheckBoxList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.checkBoxChangeHandler = this.checkBoxChangeHandler.bind(this);
+
+		this.state = {
+			selectedItems: this.props.selectedItems || []
+		};
+	}
+
+	componentWillReceiveProps(props) {
+		if (props.selectedItems) {
+			this.setState({
+				selectedItems: props.selectedItems
+			});
+		}
+	}
+
+	checkBoxChangeHandler(event) {
+		var value = event.target.value;
+		var selectedItems = this.state.selectedItems;
+
+		if (this.state.selectedItems.indexOf(value) == -1) {
+			selectedItems.push(value);
+		}
+		else {
+			selectedItems.splice(this.state.selectedItems.indexOf(value), 1);
+		}
+
+		this.setState({
+			selectedItems: selectedItems
+		}, function() {
+			if (this.props.onChange) {
+				this.props.onChange(this.state.selectedItems);
+			}
+		}.bind(this));
+	}
+
+	render() {
+		var items = this.props.values ? this.props.values.map(function(value, index) {
+			return <label key={index} className="item"><input type="checkbox" value={value} checked={this.state.selectedItems.indexOf(value) > -1} onChange={this.checkBoxChangeHandler} /> {value}</label>
+		}.bind(this)) : [];
+		return (
+			<div className="checkbox-list">
+				{items}
+			</div>
+		);
+	}
+}
