fe4946041ba12fdf05bf2f6da83329b6ec0876e0
Per Låås <per.laas@sprakochfolkminnen.se>
2019-07-09 13:46:39 +0200

    Extended OverlayWindow for use as simple overlay window for authentication: added showClose to props in OverlayWindow. false=do not show close button.

--- components/controls/OverlayWindow.js
+++ components/controls/OverlayWindow.js
@@ -4,23 +4,33 @@ import React from 'react';
 
 export default class OverlayWindow extends React.Component {
 	constructor(props) {
+		//console.log('OverlayWindow constructor');
 		super(props);
 
 		this.closeButtonClickHandler = this.closeButtonClickHandler.bind(this);
 
+		let showClose = true;
+		if (this.props.showClose) {
+			let showClose = this.props.showClose;
+		}
+
 		this.state = {
 			visible: false,
 			title: this.props.title,
+			showClose: showClose,
 			htmlContent: this.props.htmlContent
 		};
+		//console.log('OverlayWindow constructor this.state.showClose:' + this.state.showClose);
 	}
 
 	componentDidMount() {
+		//console.log('OverlayWindow didMount');
 		window.eventBus.addEventListener('overlay.intro', function(event, data) {
 			this.setState({
 				visible: true,
 				title: data && data.title ? data.title : this.state.title
 			});
+			//console.log('overlay.intro');
 		}.bind(this));
 
 		window.eventBus.addEventListener('overlay.hide', function(event) {
@@ -28,6 +38,7 @@ export default class OverlayWindow extends React.Component {
 				visible: false
 			});
 		}.bind(this));
+		//console.log('OverlayWindow didMount end');
 	}
 
 	componentWillReceiveProps(props) {
@@ -53,12 +64,13 @@ export default class OverlayWindow extends React.Component {
 	}
 
 	render() {
+		//console.log('OverlayWindow render');
 		return <div className={'overlay-container light-modal'+(this.state.visible ? ' visible' : '')}>
 			<div className="overlay-window medium">
 				
 				<div className="overlay-header">
 					{this.state.title}
-					<button className="close-button white" onClick={this.closeButtonClickHandler}></button>
+					{(this.state.showClose ? '<button className="close-button white" onClick='+(this.closeButtonClickHandler)+'></button> ' : '')} 
 				</div>
 
 				<div>
