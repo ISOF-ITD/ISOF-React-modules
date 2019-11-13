2014d38c61b506fb1c11043d7f81fb32249d6f27
Per Låås <per.laas@sprakochfolkminnen.se>
2019-11-13 14:31:19 +0100

    Byte till isof.se

--- utils/mapHelper.js
+++ utils/mapHelper.js
@@ -52,7 +52,7 @@ export default {
 	tileLayers: [
 		{
 			label: 'Lantmäteriet topografisk karta',
-			url: 'https://frigg.sprakochfolkminnen.se/sagendatabas/api/lm_proxy/{z}/{y}/{x}.png',
+			url: 'https://frigg.isof.se/sagendatabas/api/lm_proxy/{z}/{y}/{x}.png',
 			options: {
 				attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a> Topografisk Webbkarta Visning',
 				crossOrigin: true,
