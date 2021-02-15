c1023c7fec686891febc39f8fbb66ac5e6612617
Rico Simke <rico.simke@isof.se>
2021-02-15 15:58:46 +0100

    textElement is a div with class

--- components/views/RecordView.js
+++ components/views/RecordView.js
@@ -267,7 +267,7 @@ export default class RecordView extends React.Component {
 			}
 			// Annars visar vi texten som vanligt
 			else {
-				textElement = <p dangerouslySetInnerHTML={{__html: this.state.data.text}} />;
+				textElement = <div class="record-text" dangerouslySetInnerHTML={{__html: this.state.data.text}} />;
 			}
 
 			// Förbereder kategori länk
