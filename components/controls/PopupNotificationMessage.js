0a2665c3d87d866ded133d3b3c49d259d304e3b2
Per Låås <per.laas@isof.se>
2021-10-29 13:59:46 +0200

    On screen interaction remove message. Longer messages stay visible without timeout.

--- components/controls/PopupNotificationMessage.js
+++ components/controls/PopupNotificationMessage.js
@@ -7,6 +7,7 @@ export default class PopupNotificationMessage extends React.Component {
 		super(props);
 
 		this.popupNotificationReceivedHandler = this.popupNotificationReceivedHandler.bind(this);
+		this.popupNotificationCloseHandler = this.popupNotificationCloseHandler.bind(this);
 
 		this.state = {
 			message: null,
@@ -16,37 +17,52 @@ export default class PopupNotificationMessage extends React.Component {
 
 	componentDidMount() {
 		window.eventBus.addEventListener('popup-notification.notify', this.popupNotificationReceivedHandler);
+		window.eventBus.addEventListener('screen-clicked', this.popupNotificationCloseHandler);
+	}
+
+	// On screen interaction remove message
+	popupNotificationCloseHandler() {
+		this.setState({
+			visible: false
+		})
 	}
 
 	popupNotificationReceivedHandler(event, message) {
 		if (message && message != '') {
-			var setMessage = function() {
-				this.setState({
+			//Longer messages stay visible without timeout: Naive solution to identify message with many lines using line break <br>:  
+			if (message.indexOf('<br>') > 0) {
+					this.setState({
 					message: message,
 					visible: true
-				}, function() {
-					this.timeout = setTimeout(function() {
-						this.setState({
-							visible: false
-						});
-						this.timeout = null;
-					}.bind(this), this.props.duration || 5000);
-				}.bind(this));
-			}.bind(this);
+				})
+			} else {
+				var setMessage = function() {
+					this.setState({
+						message: message,
+						visible: true
+					}, function() {
+						this.timeout = setTimeout(function() {
+							this.setState({
+								visible: false
+							});
+							this.timeout = null;
+						}.bind(this), this.props.duration || 5000);
+					}.bind(this));
+				}.bind(this);
+				if (this.timeout) {
+					clearTimeout(this.timeout);
 
-			if (this.timeout) {
-				clearTimeout(this.timeout);
-
-				this.setState({
-					visible: false
-				}, function() {
-					setTimeout(function() {
-						setMessage();
-					}.bind(this), 600);
-				}.bind(this));
-			}
-			else {
-				setMessage();
+					this.setState({
+						visible: false
+					}, function() {
+						setTimeout(function() {
+							setMessage();
+						}.bind(this), 600);
+					}.bind(this));
+				}
+				else {
+					setMessage();
+				}
 			}
 		}
 	}
