9e2b6072a583881cfcc66063867097260ff786f5
Per Låås <per.laas@isof.se>
2021-11-19 13:01:00 +0100

    New transcription help text.

--- components/views/TranscriptionHelpOverlay.js
+++ components/views/TranscriptionHelpOverlay.js
@@ -59,14 +59,23 @@ export default class TranscriptionHelpOverlay extends React.Component {
 				<p>Vill du hjälpa till att tillgängliggöra samlingarna? Här finns möjligheten att skriva av berättelser, t.ex. från en viss ort eller ett visst ämne. Detta gör inte bara berättelserna mer tillgängliga för fler personer utan möjliggör också fritextsökningar.</p>
 				<p><strong>Vilken information i vilken ruta? </strong></p>
 				<p>Bakom varje fält finns symbolen X. Genom att trycka på den får du information om vilken information som ska skrivas in i fältet ifråga.</p>
+				<p>Håll muspekaren över respektive tomt textfält. Den efterfrågade informationen ringas in av en röd ring, vilket gör att du enkelt kan hitta informationen i uppteckningen till höger</p>
 				<p><strong>Oläsliga ord?</strong></p>
-				<p>Om du inte kan läsa något ord på uppteckning, ange ”###” istället för ordet ifråga.</p>
+				<p>Om du inte kan läsa något ord på uppteckningen, ange ”###” istället för ordet ifråga. Ange ”###” för alla sorters ord, oavsett hur många bokstäver det är i det. Är det flera ord bredvid varandra som inte kan tydas, skriv ”###” för varje ord.</p>
+				<p><strong>Ny rad?</strong></p>
+				<p>Skriv av raderna exakt som de står. Även stavfel. Om texten radbryts i uppteckningen, klicka då på Enter för att börja en ny rad i textfältet.</p>
+				<p><strong>Text i marginalen?</strong></p>
+				<p>Ibland har upptecknaren fortsatt på en mening nedanför raderna på blanketten. Skriv med det i textfältet. Övriga ord eller tecken som ligger i marginalen men som inte hör till själva uppteckningen behöver du inte transkribera.</p>
 				<p><strong>Flera sidor? </strong></p>
-				<p>Består uppteckningen av flera sidor får du gärna skriva in samtliga sidor i samma ruta.</p>
+				<p>Består uppteckningen av flera sidor får du gärna skriva in samtliga sidor i samma textruta, men markera när du börjar skriva på en annan sida med tecknet ”/”. Sidorna ligger som en lista under den aktuella sidan. Tryck på en sida för att få den stor.</p>
+				<p><strong>Zooma</strong></p>
+				<p>Vill du se ännu tydligare vad det står i uppteckningen? Det går bra att zooma in och ut. Använd zoom-funktionen i övre vänstra hörnet och flytta fokus på sidan genom att röra muspekaren.</p>
 				<p><strong>Vilka är personerna som nämns i texten?</strong></p>
-				<p>Känner du till någon av personerna som omnämns i uppteckningen? Kontakta oss knappen "Vet du mer" på uppteckningen. </p>
+				<p>Känner du till någon av personerna som omnämns i uppteckningen? Kontakta oss via knappen "Vet du mer" på uppteckningen.</p>
+				<p><strong>Kommentar till avskriften</strong></p>
+				<p>Har du stött på något problem med avskriften, eller har du någon annan kommentar till den? Skriv då i kommentarsfältet.</p>
 				<p><strong>Vad händer efter att du tryckt på ”Skicka in?”</strong></p>
-				<p>Efter kvalitetssäkring kommer texten sedan att publiceras i applikationen och införlivas i Isofs digitala arkiv. </p>
+				<p>Efter kvalitetssäkring kommer texten sedan att publiceras på Folke och införlivas i Isofs digitala arkiv.</p>
 				<p><a href="https://www.isof.se/arkiv-och-insamling/digitala-arkivtjanster/folke/transkribera"><strong>{l('Läs mer om att skriva av.')}</strong></a><br/><br/></p>
 
 			</div>;
