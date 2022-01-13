9e19cad3ad6c28b3ea7b595d13f5f96c9dbee8f7
Per Låås <per.laas@isof.se>
2022-01-13 16:03:25 +0100

    Less margins in transcribe help text

--- components/views/TranscriptionHelpOverlay.js
+++ components/views/TranscriptionHelpOverlay.js
@@ -54,31 +54,29 @@ export default class TranscriptionHelpOverlay extends React.Component {
 			</div>;
 		}
 		else {
-			var overlayContent = <div>
-
+			var overlayContent = <div className='helptext'>
 				<p>Vill du hjälpa till att tillgängliggöra samlingarna? Här finns möjligheten att skriva av berättelser, t.ex. från en viss ort eller ett visst ämne. Detta gör inte bara berättelserna mer tillgängliga för fler personer utan möjliggör också fritextsökningar.</p>
 				<p><strong>Vilken information i vilken ruta? </strong></p>
-				<p>Varje textfält som ska fyllas i motsvarar en särskild rad i uppteckningen som du ser på höger sida. Ibland saknas text på vissa rader. Lämna då samma textfält tomt.</p>
+				Varje textfält som ska fyllas i motsvarar en särskild rad i uppteckningen som du ser på höger sida. Ibland saknas text på vissa rader. Lämna då samma textfält tomt.
 				<p><strong>Oläsliga ord?</strong></p>
-				<p>Om du inte kan läsa något ord på uppteckningen, ange ”###” istället för ordet ifråga. Är det flera ord bredvid varandra som inte kan tydas, skriv ”###” för varje ord.</p>
+				Om du inte kan läsa något ord på uppteckningen, ange ”###” istället för ordet ifråga. Är det flera ord bredvid varandra som inte kan tydas, skriv ”###” för varje ord.
 				<p><strong>Ny rad?</strong></p>
-				<p>Skriv av raderna exakt som de står, även stavfel. Överstrukna ord kan ignoreras. Om texten radbryts i uppteckningen, klicka då på Enter för att påbörja en ny rad i textfältet.</p>
+				Skriv av raderna exakt som de står, även stavfel. Överstrukna ord kan ignoreras. Om texten radbryts i uppteckningen, klicka då på Enter för att påbörja en ny rad i textfältet.
 				<p><strong>Specialtecken?</strong></p>
-				<p>Ibland förekommer specialtecken i uppteckningarna. I den mån du kan, skriv även dessa tecken med i avskriften. Det kan till exempel vara ô, skrivet som ett o med ^ över.</p>
+				Ibland förekommer specialtecken i uppteckningarna. I den mån du kan, skriv även dessa tecken med i avskriften. Det kan till exempel vara ô, skrivet som ett o med ^ över.
 				<p><strong>Text i marginalen?</strong></p>
-				<p>Ibland har upptecknaren fortsatt på en mening nedanför raderna på blanketten. Skriv med det i textfältet. Övriga ord eller tecken som ligger i marginalerna men som inte hör till själva uppteckningen, behöver du inte transkribera.</p>
+				Ibland har upptecknaren fortsatt på en mening nedanför raderna på blanketten. Skriv med det i textfältet. Övriga ord eller tecken som ligger i marginalerna men som inte hör till själva uppteckningen, behöver du inte transkribera.
 				<p><strong>Flera sidor? </strong></p>
-				<p>Består uppteckningen av flera sidor skriver du in samtliga sidor i samma textruta, men markera när du börjar skriva på en annan sida med tecknet ”/”. Sidorna ligger som en lista under den aktuella sidan. Tryck på en sida för att förstora.</p>
+				Består uppteckningen av flera sidor skriver du in samtliga sidor i samma textruta, men markera när du börjar skriva på en annan sida med tecknet ”/”. Sidorna ligger som en lista under den aktuella sidan. Tryck på en sida för att förstora.
 				<p><strong>Zooma</strong></p>
-				<p>Vill du se ännu tydligare vad det står i uppteckningen? Det går bra att zooma in och ut. Använd zoom-funktionen i övre vänstra hörnet och flytta fokus på sidan genom att röra muspekaren.</p>
+				Vill du se ännu tydligare vad det står i uppteckningen? Det går bra att zooma in och ut. Använd zoom-funktionen i övre vänstra hörnet och flytta fokus på sidan genom att röra muspekaren.
 				<p><strong>Vilka är personerna som nämns i texten?</strong></p>
-				<p>Känner du till någon av personerna som omnämns i uppteckningen och har du eventuellt fotografier eller liknande? Kontakta oss via knappen "Vet du mer" ovanför uppteckningen. Du kan även välja att skriva en kommentar om personen i kommentarsfältet.</p>
+				Känner du till någon av personerna som omnämns i uppteckningen och har du eventuellt fotografier eller liknande? Kontakta oss via knappen "Vet du mer" ovanför uppteckningen. Du kan även välja att skriva en kommentar om personen i kommentarsfältet.
 				<p><strong>Kommentar till avskriften</strong></p>
-				<p>Har du stött på något problem med avskriften eller har du någon annan kommentar till den? Skriv då i kommentarsfältet.</p>
+				Har du stött på något problem med avskriften eller har du någon annan kommentar till den? Skriv då i kommentarsfältet.
 				<p><strong>Vad händer efter att du tryckt på ”Skicka in?”</strong></p>
-				<p>Efter kvalitetssäkring kommer texten sedan att publiceras på Folke och införlivas i Isofs digitala arkiv.</p>
-				<p><a href="https://www.isof.se/arkiv-och-insamling/digitala-arkivtjanster/folke/transkribera"><strong>{l('Läs mer om att skriva av.')}</strong></a><br/><br/></p>
-
+				Efter kvalitetssäkring kommer texten sedan att publiceras på Folke och införlivas i Isofs digitala arkiv.
+				<br/><a href="https://www.isof.se/arkiv-och-insamling/digitala-arkivtjanster/folke/transkribera"><strong>{l('Läs mer om att skriva av.')}</strong></a><br/><br/>
 			</div>;
 		}
 
