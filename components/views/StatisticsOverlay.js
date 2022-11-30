82aa7373910d2e7c55e357d167d69e5cbb115eb6
Rico Simke <rico.simke@isof.se>
2022-11-30 11:45:49 +0100

    button improvement

---
+++ components/views/StatisticsOverlay.js
@@ -0,0 +1,10 @@
+import React from "react";
+import config from "./../../../scripts/config.js";
+
+export default class StatisticsOverlay extends React.Component {
+    render() {
+        return(
+            <span>Hello World!</span>
+        )
+    }
+}
--- components/views/TranscribeButton.js
+++ components/views/TranscribeButton.js
@@ -12,14 +12,44 @@ export default class TranscribeButton extends React.Component {
 
 	transcribeButtonClick() {
 		if (window.eventBus) {
-			window.eventBus.dispatch('overlay.transcribe', {
-				url: config.siteUrl+'#/records/'+this.props.recordId,
-				id: this.props.recordId,
-				title: this.props.title,
-				type: this.props.type,
-				images: this.props.images,
-				transcriptionType: this.props.transcriptionType,
-			});
+			if(this.props.random) {
+				let randomDocument = {};
+				console.log("r");
+				// get random document and save its data to current state
+				// todo: create params with required params from config
+				fetch("http://localhost:5000/api/es/random_document/?type=arkiv&recordtype=one_record&transcriptionstatus=readytotranscribe&mark_metadata=transcriptionstatus&categorytypes=tradark&publishstatus=published")
+				.then(function(response) {
+								return response.json()
+							})
+							.then(function(json) {
+								randomDocument = json.hits.hits[0]._source;
+								console.log(randomDocument);
+								window.eventBus.dispatch('overlay.transcribe', {
+									url: config.siteUrl+'#/records/'+randomDocument.id,
+									id: randomDocument.id,
+									archiveId: randomDocument.archive.archive_id,
+									title: randomDocument.title,
+									images: randomDocument.media,
+									transcriptionType: randomDocument.transcriptiontype,
+								});
+								})
+								.then(function(json) {
+								}.bind(this))
+								.catch(function(ex) {
+									console.log('parsing failed', ex)
+								});
+			}
+			else {
+				window.eventBus.dispatch('overlay.transcribe', {
+					url: config.siteUrl+'#/records/'+this.props.recordId,
+					id: this.props.recordId,
+					title: this.props.title,
+					type: this.props.type,
+					images: this.props.images,
+					transcriptionType: this.props.transcriptionType,
+				});
+			}
+			
 		}
 	}
 
