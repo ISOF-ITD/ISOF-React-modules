1d7d20d8947202b61c0e9a96f7f3c3422ab363bc
Rico Simke <rico.simke@isof.se>
2023-02-14 14:33:23 +0100

    TranscriptionHelpOverlay.js

--- components/views/TranscriptionHelpOverlay.js
+++ components/views/TranscriptionHelpOverlay.js
@@ -59,10 +59,10 @@ export default class TranscriptionHelpOverlay extends React.Component {
 				<p><strong>Vilken information i vilken ruta? </strong></p>
 				Varje textfält som ska fyllas i motsvarar en särskild rad i uppteckningen som du ser på höger sida. Ibland saknas text på vissa rader. Lämna då samma textfält tomt.
 				<p><strong>Om berättaren</strong></p>
-				Informationen som hör till berättaren börjar med fältet ”Berättat av”. I det textfältet skriver du in namnet på personen. Ibland kan det stå något mer direkt efter namnet, t.ex. namnet på en gård eller en ort. Ange den informationen istället i fältet ”Fält under berättat av”. <br/>
-				Kortfattat kan man säga att all information om berättaren som inte är ett namn (Berättat av), ett födelseår (Född år), eller en födelseort (Född i) kan skrivas i fältet ”Fält under berättat av”. 
+				Informationen som hör till berättaren börjar med fältet <strong>”Berättat av”</strong>. I det textfältet skriver du in namnet på personen. Ibland kan det stå något mer direkt efter namnet, t.ex. namnet på en gård eller en ort. Ange den informationen istället i fältet <strong>”Fält under berättat av”</strong>. <br/>
+				Kortfattat kan man säga att all information om berättaren som inte är ett namn (Berättat av), ett födelseår (Född år), eller en födelseort (Född i) kan skrivas i fältet <strong>”Fält under berättat av”</strong>. 
 				<p><strong>Oläsliga ord?</strong></p>
-				Om du inte kan läsa något ord på uppteckningen, ange <strong>###</strong> istället för ordet ifråga. Är det flera ord bredvid varandra som inte kan tydas, skriv ### för varje ord.
+				Om du inte kan läsa något ord på uppteckningen, ange <strong>###</strong> istället för ordet ifråga. Är det flera ord bredvid varandra som inte kan tydas, skriv <strong>###</strong> för varje ord.
 				<p><strong>Ny rad?</strong></p>
 				Skriv av raderna exakt som de står, även med radbrytningar och stavfel. Överstrukna ord kan ignoreras. Om texten radbryts i uppteckningen, klicka då på Enter för att påbörja en ny rad i textfältet.
 				<p><strong>Specialtecken?</strong></p>
@@ -70,7 +70,7 @@ export default class TranscriptionHelpOverlay extends React.Component {
 				<p><strong>Text i marginalen?</strong></p>
 				Ibland har upptecknaren fortsatt på en mening nedanför raderna på blanketten. Skriv med det i textfältet. Övriga ord eller tecken som ligger i marginalerna men som inte hör till själva uppteckningen, behöver du inte transkribera. Däremot kan du skriva med marginaltexten som en kommentar till uppteckning.
 				<p><strong>Flera sidor?</strong></p>
-				Består uppteckningen av flera sidor skriver du in samtliga sidor i samma textruta, men markera när du börjar skriva på en annan sida med tecknet <strong>/</strong> (snedstreck). Tryck på Enter innan och efter /. Snedstrecket ska alltså stå ensamt på en egen rad. <br/> Sidorna ligger som en lista under den aktuella sidan. Tryck på en sida för att förstora.
+				Består uppteckningen av flera sidor skriver du in samtliga sidor i samma textruta, men markera när du börjar skriva på en annan sida med tecknet <strong>/</strong> (snedstreck). Tryck på Enter innan och efter <strong>/</strong>. Snedstrecket ska alltså stå ensamt på en egen rad. <br/> Sidorna ligger som en lista under den aktuella sidan. Tryck på en sida för att förstora.
 				<p><strong>Zooma</strong></p>
 				Vill du se ännu tydligare vad det står i uppteckningen? Det går bra att zooma in och ut. Använd zoom-funktionen i övre vänstra hörnet och flytta fokus på sidan genom att röra muspekaren.
 				<p><strong>Vilka är personerna som nämns i texten?</strong></p>
